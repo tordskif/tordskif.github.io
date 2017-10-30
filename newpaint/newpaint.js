90d7a22e0a7bef1a90f4c104763c8384a79fe0ce
Drotkle <32832651+Drotkle@users.noreply.github.com>
2017-10-30 14:02:10 +0100

    new commit

--- newpaint/newpaint.js
+++ newpaint/newpaint.js
@@ -7,6 +7,11 @@ function setup() {
     canvas.addEventListener("mousemove", go);
     canvas.addEventListener("click", go);
 
+    let rotateSlider = document.getElementById("rota");
+    rotateSlider.addEventListener("input", function () { document.getElementById("slidervalue").innerHTML = rotateSlider.value; });
+    let polygon = document.getElementById("polygon");
+    polygon.addEventListener("input", function () { if (polygon.value < 3){polygon.value = 3}});
+
     let rekt = document.getElementById("rektangel");
     rekt.addEventListener("click", changebrush);
     let tri = document.getElementById("trekant");
@@ -16,15 +21,17 @@ function setup() {
     let visk = document.getElementById("visk");
     visk.addEventListener("click", changebrush);
 
-    document.getElementById("size").value = 20;
+    document.getElementById("size").value = 40;
+    polygon.value = 3;
 
     function go(e) {
         counter++;
-        if(e.altKey) {
+        if (e.altKey) {
             counter = -1000000;
         }
         let strokesize = document.getElementById("size").value;
         let color = document.getElementById("color").value;
+        let rota = (rotateSlider.value - 90) * Math.PI / 180;
         if (e.buttons === 1 || e.type === "click") {
             if (valg === "rektangel") {
                 ctx.fillStyle = color;
@@ -40,16 +47,18 @@ function setup() {
                 ctx.fill();
             }
             if (valg === "trekant") {
+                //for any polygon instead:
                 ctx.beginPath();
-                ctx.moveTo(e.layerX - strokesize / 2, e.layerY + (strokesize / 2) * Math.tan(Math.PI / 6));
-                ctx.lineTo((e.layerX + strokesize / 2), e.layerY + (strokesize / 2) * Math.tan(Math.PI / 6));
-                ctx.lineTo(e.layerX, e.layerY - strokesize * Math.sin(Math.PI / 3) + (strokesize / 2) * Math.tan(Math.PI / 6));
+                ctx.moveTo(e.layerX + strokesize * Math.cos(Math.PI * 0 + rota) / 2, e.layerY + strokesize * Math.sin(Math.PI * 0 + rota) / 2);
+                for (i = 1; i < polygon.value; i++) {
+                    ctx.lineTo(e.layerX + strokesize * Math.cos(Math.PI * 2 * i / polygon.value + rota) / 2, e.layerY + strokesize * Math.sin(Math.PI * 2 * i / polygon.value + rota) / 2);
+                }
                 ctx.closePath();
                 ctx.fillStyle = color;
                 ctx.fill();
             }
         }
-        if(counter > 250) {
+        if (counter > 250) {
             img.style.visibility = "visible";
         }
     }
@@ -57,8 +66,13 @@ function setup() {
         valg = e.path[0].id;
         let all = document.getElementsByClassName("option");
         for (var i = 0; i < all.length; i++) {
-          all[i].style.backgroundColor = "white";
+            all[i].style.backgroundColor = "white";
         }
-        e.path[0].style.backgroundColor = "#EAEA88";
+        if (valg === "polygon") {
+            valg = e.path[1].id;
+            e.path[1].style.backgroundColor = "#EAEA88";
+        } else {
+            e.path[0].style.backgroundColor = "#EAEA88";
+        }
     }
 }
