dfd8d4b892a1bf93c0122b2ffaa96e50ac0a0bb9
Drotkle <32832651+Drotkle@users.noreply.github.com>
2017-12-07 15:42:50 +0100

    added wincounter i think

--- 4inrow/4inrowOnlyWin.js
+++ 4inrow/4inrowOnlyWin.js
@@ -9,7 +9,7 @@ Will then end up with the result which minimizes the opponens score while maximi
 */
 
 function setup() {   
-    localStorage.setItem("record", "0,0");
+    //localStorage.setItem("record", "0,0");
     let record = localStorage.getItem("record");
     let gameState = 0;
     let gameBoard = [
