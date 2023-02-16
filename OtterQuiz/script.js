0456044ce194fddb1771b605df468070554f3dcc
Tord <t_skiftestad@live.no>
2023-02-16 20:53:24 +0100

    added test button

--- OtterQuiz/script.js
+++ OtterQuiz/script.js
@@ -3,6 +3,6 @@
 let videoPlayer = document.getElementById("videoPlayer")
 
 function setVideo(src) {
-    srcString = "https://player.twitch.tv/?channel=marinemammalrescue&parent=tordskif.github.io&autoplay=false"
+    srcString = "https://player.twitch.tv/?channel=numotthenummy&parent=tordskif.github.io&autoplay=false"
     videoPlayer.src = srcString
 }
