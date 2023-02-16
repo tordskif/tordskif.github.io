c9c49470579aecb3239e4c8e8d124a300bc7d4da
Tord <t_skiftestad@live.no>
2023-02-16 20:49:08 +0100

    fixed error in src string

--- OtterQuiz/script.js
+++ OtterQuiz/script.js
@@ -1,1 +1,8 @@
 // parent=index.html&
+
+let videoPlayer = document.getElementById("videoPlayer")
+
+function setVideo(src) {
+    srcString = "https://player.twitch.tv/?channel=marinemammalrescue&parent=tordskif.github.io&autoplay=false"
+    videoPlayer.src = srcString
+}
